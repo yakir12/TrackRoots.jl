14a2cffff30cadc7921927f362959e622dc35a3a
yakir12 <12.yakir@gmail.com>
2018-02-13 19:44:09 +0100

    cleaned some

--- src/miscsetup.jl
+++
@@ -1,8 +0,0 @@
-using Images, Kalman, GaussianDistributions
-
-import Base.LinAlg.normalize!
-function normalize!(x::AbstractArray)
-    l, u = extrema(x)
-    (x - l)/(u - l)
-end
-greyscale(A) = map(x->RGB(x,x,x), normalize!(copy(A)));
