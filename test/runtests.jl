87af045a67185bb5119a8bc1ef376e09df4a3f48
yakir12 <12.yakir@gmail.com>
2018-06-07 19:48:57 +0200

    a

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,8 @@ using Base.Test
 using DataDeps
 
 # # incase there are old test files, remove them just in case they are old/bad
-rm(joinpath(first(DataDeps.default_loadpath), "test"), recursive=true, force=true)
+# rm(joinpath(first(DataDeps.default_loadpath), "test"), recursive=true, force=true)
+rm(joinpath(first(DataDeps.default_loadpath), "test", "1", "d2"), recursive=true, force=true)
 
 ENV["DATADEPS_ALWAY_ACCEPT"]=true
 register(DataDep("test",
@@ -70,12 +71,12 @@ end
     ndfile = joinpath(datadep"test", "1", "d2.nd")
     main(ndfile, startpoint, Base.DevNullStream())
 
-    #=@test isfile(joinpath(datadep"test", "1", "d2", "stage 1", "roots.png"))
+    @test isfile(joinpath(datadep"test", "1", "d2", "stage 1", "roots.png"))
     @test isfile(joinpath(datadep"test", "1", "d2", "stage 1", "root 1", "coordinates.csv"))
     @test isfile(joinpath(datadep"test", "1", "d2", "stage 1", "root 1", "intensities.csv"))
     @test isfile(joinpath(datadep"test", "1", "d2", "stage 1", "root 1", "summary.mp4"))
 
     #clean up
-    rm(joinpath(first(DataDeps.default_loadpath), "test", "1", "d2"), recursive=true, force=true)=#
+    #=rm(joinpath(first(DataDeps.default_loadpath), "test", "1", "d2"), recursive=true, force=true)=#
 end
 
