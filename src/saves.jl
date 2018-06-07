a7b17b123139607456f4e7793f315e0d7f975507
yakir12 <12.yakir@gmail.com>
2018-06-07 20:01:04 +0200

    no for

--- src/saves.jl
+++ src/saves.jl
@@ -75,11 +75,11 @@ function saveoverview(img::Matrix{Float64}, formatlabel::Function, rs::Vector{Tr
     mM = quantile(vec(img), [.5, .995])
     img .= imadjustintensity(img, mM)
     heatmap(flipdim(img, 1), aspect_ratio = 1, yformatter = formatlabel, xformatter = formatlabel, xlabel = "X (mm)", ylabel = "Y (mm)", color=:inferno, yflip=true, colorbar=false, legend=false, size=(sz,sz), dpi=50/3)
-    for r in rs
+    #=for r in rs
         x = last.(r.coordinates)
         y = first.(r.coordinates)
         plot!(x, y, color=r.color, linestyle = :dot, linewidth = 5, linewidth = intensity_radius, annotations=(x[1],y[1],text(string(r.id), :left, r.color, 25)))
-    end
+    end=#
     png(joinpath(path, "roots"))
 end
 
