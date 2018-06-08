45dc5b86bc01b3180bba3f27bb65859eed3bcbe6
yakir12 <12.yakir@gmail.com>
2018-06-08 21:46:14 +0200

    plot4

--- src/mwe.jl
+++ src/mwe.jl
@@ -11,13 +11,20 @@ img = rand(100,100)
 formatlabel(x) = 2x+1
 rs = [Track([Mark(rand(), rand()) for j in 1:100], rand(RGB{Float64}), rand(1:10)) for i in 1:5]
 path = homedir()
-img .= adjust_gamma(img, 2.4)
-mM = quantile(vec(img), [.5, .995])
-img .= imadjustintensity(img, mM)
-heatmap(flipdim(img, 1), aspect_ratio = 1, yformatter = formatlabel, xformatter = formatlabel, xlabel = "X (mm)", ylabel = "Y (mm)", color=:inferno, yflip=true, colorbar=false, legend=false, size=(sz,sz), dpi=50/3)
-for r in rs
-    x = last.(r.coordinates)
-    y = first.(r.coordinates)
-    plot!(x, y, color=r.color, linestyle = :dot, linewidth = 5, linewidth = intensity_radius, annotations=(x[1],y[1],text(string(r.id), :left, r.color, 25)))
+function saveoverview(img::Matrix{Float64}, formatlabel::Function, rs::Vector{Track}, path::String)
+    img .= adjust_gamma(img, 2.4)
+    mM = quantile(vec(img), [.5, .995])
+    img .= imadjustintensity(img, mM)
+    heatmap(flipdim(img, 1), aspect_ratio = 1, yformatter = formatlabel, xformatter = formatlabel, xlabel = "X (mm)", ylabel = "Y (mm)", color=:inferno, yflip=true, colorbar=false, legend=false, size=(sz,sz), dpi=50/3)
+    for r in rs
+        x = last.(r.coordinates)
+        y = first.(r.coordinates)
+        plot!(x, y, color=r.color, linestyle = :dot, linewidth = 5, linewidth = intensity_radius, annotations=(x[1],y[1],text(string(r.id), :left, r.color, 25))) # works
+    end
+    png(joinpath(path, "roots"))
 end
-png(joinpath(path, "roots"))
+saveoverview(img, formatlabel, rs, path)
+
+
+
+
